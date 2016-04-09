c8dbb72ffe29a743eea34a4d207fa5d30ebe998e
Stadzior <Stadzior@gmail.com>
2016-04-09 11:49:43 +0200

    Added ShoppingCart test class with empty test method stub.

---
+++ E02.DependencyBreaking/Tests/ShoppingCartTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E03.DependencyBreaking.Tests
+{
+    class ShoppingCartTests
+    {
+        [Test]
+        public void DrinkingBeer_ShouldBeAllowed_WhenAllTestsPass()
+        {
+            // Arrange
+
+
+            // Act
+
+            // Assert
+        }
+    }
+}
