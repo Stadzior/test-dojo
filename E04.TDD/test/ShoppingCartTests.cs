3803c4a85fb685e4ddfd68186c13d6adc2dff4e5
Stadzior <Stadzior@gmail.com>
2016-06-18 17:04:53 +0200

    Charge should be 100 when cart contains 2 items which costs 50.

--- E04.TDD/test/ShoppingCartTests.cs
+++ E04.TDD/test/ShoppingCartTests.cs
@@ -2,12 +2,20 @@
 using E02.TDD.main;
 using NUnit.Framework;
 using Shouldly;
+using Moq;
 
 namespace E02.TDD.test
 {
+    [TestFixture]
     class ShoppingCartTests
     {
-        [Test]
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+        }
+
+    [Test]
         public void Charge_ShouldBeZero_WhenCartIsEmpty()
         {
             // Arrange
@@ -18,6 +26,21 @@ namespace E02.TDD.test
 
             // Assert
             cart.Payment.Value.ShouldBe(0.0);
+        }
+
+        [Test]
+        public void Charge_ShouldBeOneHundred_WhenCartContainsTwoProducts_WhichCostsFifty()
+        {
+            // Arrange
+            Item item = a(Item().withPrice(50.0).withQuantity(2));
+            ItemCollection items = a(Items().withItem(item));
+            ShoppingCart cart = a(Cart().withItems(items));
+
+            // Act
+            cart.Checkout();
+
+            // Assert
+            cart.Payment.Value.ShouldBe(100.0);
         }
 
         private ShoppingCartBuilder Cart()
