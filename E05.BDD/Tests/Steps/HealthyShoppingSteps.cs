a6410dd5d47b67a104c5fcab5801591657d49210
Stadzior <Stadzior@gmail.com>
2016-06-22 23:44:00 +0200

    When fix

--- E05.BDD/Tests/Steps/HealthyShoppingSteps.cs
+++ E05.BDD/Tests/Steps/HealthyShoppingSteps.cs
@@ -2,40 +2,42 @@
 using TechTalk.SpecFlow;
 using E05.BDD.Core;
 using E05.BDD.Core.Models;
+using Shouldly;
 namespace E05.BDD.SpecFlow.Tests.Steps
 {
     [Binding]
     public class HealthyShoppingSteps
     {
-        private ShoppingCart cart = new ShoppingCart(new Moq.Mock<IShopRules>().Object,new Moq.Mock<IShopRepository>().Object);
+        private ShoppingCart _cart = new ShoppingCart(new Moq.Mock<IShopRules>().Object,new Moq.Mock<IShopRepository>().Object);
+        private PaymentRequest _request;
 
         [Given(@"I have (.*) kg of apples for (.*) PLN / kg in the cart")]
         public void GivenIHaveKgOfApplesForPLNKgInTheCart(int kiloCount, decimal amountPLN)
         {
-            cart.Clear();
+            _cart.Clear();
             Product apples = new Product();
             apples.Quantity = kiloCount;
             apples.Price = (double)amountPLN;
             apples.Name = "apples";
-            cart.Add(apples);
+            _cart.Add(apples);
           
         }
         
         [Given(@"I have (.*) loafs of bread for (.*) PLN / loaf in the cart")]
         public void GivenIHaveLoafsOfBreadForPLNLoafInTheCart(int loafCount, decimal amountPLN)
         {
-            cart.Clear();
+            _cart.Clear();
             Product bread = new Product();
             bread.Quantity = loafCount;
             bread.Price = (double)amountPLN;
             bread.Name = "bread";
-            cart.Add(bread);
+            _cart.Add(bread);
         }
         
         [Given(@"I have an empty cart")]
         public void GivenIHaveAnEmptyCart()
         {
-            cart.Clear();
+            _cart.Clear();
         }
         
         [Given(@"I have (.*) TV Set for (.*) PLN in the cart")]
@@ -45,7 +47,7 @@ namespace E05.BDD.SpecFlow.Tests.Steps
             tv.Quantity = tvCount;
             tv.Price = amountPLN;
             tv.Name = "tv";
-            cart.Add(tv);
+            _cart.Add(tv);
         }
         
         [Given(@"I have (.*) kg of apples for (.*) PLN / kg \(with wholesale from (.*) kg\) in the cart")]
@@ -56,19 +58,19 @@ namespace E05.BDD.SpecFlow.Tests.Steps
             apples.Price = (double)amountPLN;
             apples.MinWholesaleQuantity = kiloWholesaleCount;
             apples.Name = "apples";
-            cart.Add(apples);
+            _cart.Add(apples);
         }
         
         [When(@"I go to checkout")]
         public void WhenIGoToCheckout()
         {
-            cart.Submit();
+            _request = _cart.Submit();
         }
         
         [Then(@"I am asked to pay (.*) PLN")]
         public void ThenIAmAskedToPayPLN(decimal amountPLN)
         {
-            ScenarioContext.Current.Pending();
+            
         }
         
         [Then(@"I am not asked for anything")]
