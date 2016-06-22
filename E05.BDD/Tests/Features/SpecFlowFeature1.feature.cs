e038778ebd3c74f850b531ebe130a1fcebbb1943
Stadzior <Stadzior@gmail.com>
2016-06-22 22:13:46 +0200

    Moving Features to Tests directory.

--- E05.BDD/Tests/Features/SpecFlowFeature1.feature.cs
+++ E05.BDD/Tests/Features/SpecFlowFeature1.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace E05.BDD.SpecFlow.Core.Features
+namespace E05.BDD.SpecFlow.Tests.Features
 {
     using TechTalk.SpecFlow;
     
